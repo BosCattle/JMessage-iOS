be2c54bd3f5c5e3514bbaae622c409a564208847
jiangTaoQuite <jiangtao103cp@163.com>
2017-06-30 01:38:48 +0800

    init

---
+++ JMessage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JMessage
+//
+//  Created by 姜陶 on 30/06/2017.
+//  Copyright © 2017 tech.jiangtao.ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '8.0'
+use_frameworks!
+
+inhibit_all_warnings!
+
+target 'JMessage' do
+  pod 'XMPPFramework', '~> 3.7.0'
+end
