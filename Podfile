e387aefcc247037db6488be05a37886266894cee
jiangTaoQuite <jiangtao103cp@163.com>
2017-06-30 01:53:02 +0800

    add depend

--- Podfile
+++ Podfile
@@ -5,4 +5,7 @@ inhibit_all_warnings!
 
 target 'JMessage' do
   pod 'XMPPFramework', '~> 3.7.0'
+  pod 'SwiftyJSON'
+  pod 'Alamofire', '~> 4.4'
+  pod 'SnapKit', '~> 3.2.0'
 end
--- Pods/Target Support Files/Pods-JMessage/Pods-JMessage-frameworks.sh
+++ Pods/Target Support Files/Pods-JMessage/Pods-JMessage-frameworks.sh
@@ -89,15 +89,21 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CocoaAsyncSocket/CocoaAsyncSocket.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CocoaLumberjack/CocoaLumberjack.framework"
   install_framework "$BUILT_PRODUCTS_DIR/KissXML/KissXML.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SnapKit/SnapKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
   install_framework "$BUILT_PRODUCTS_DIR/XMPPFramework/XMPPFramework.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CocoaAsyncSocket/CocoaAsyncSocket.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CocoaLumberjack/CocoaLumberjack.framework"
   install_framework "$BUILT_PRODUCTS_DIR/KissXML/KissXML.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SnapKit/SnapKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
   install_framework "$BUILT_PRODUCTS_DIR/XMPPFramework/XMPPFramework.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
